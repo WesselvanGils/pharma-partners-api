944085b707201f95dc22c0910e73b4fef9dcaa50
JoeydeJonge <jd.dejonge@student.avans.nl>
2021-12-15 13:29:26 +0100

    PPAPI- Patient is now done and now begin to meeting

--- src/models/patient.model.js
+++ src/models/patient.model.js
@@ -7,11 +7,43 @@ const getModel = require('./model_cache')
 const PatientSchema = new Schema({
     BSN: {
         type: String,
-        required: [true, 'A employee needs to have a firstname.'],
+        required: [true, 'A patient needs to have a BSN.'],
+    },
+    firstname: {
+        type: String,
+        required: [true, 'A patient needs to have a firstname.'],
+    },
+    lastname: {
+        type: String,
+        required: [true, 'A patient needs to have a lastname.'],
+    },
+    adress: {
+        type: String,
+        required: [true, 'A patient needs to have a lastname.'],
+    },
+    prefix: {
+        type: String,
+        required: [true, 'A patient needs to have a lastname.'],
+    },
+    patientNumber: {
+        type: String,
+        required: [true, 'A patient needs to have a lastname.'],
+    },
+    gender: {
+        type: String,
+        required: [true, 'A patient needs to have a gender.'],
+    },
+    dateofbirth: {
+        type: Date,
+        required: [true, 'A patient needs to have a gender.'],
+    },
+    phonenumber: {
+        type: String,
+        required: [true, 'A patient needs to have a phonenumber.'],
     },
 })
 
-PatientSchema.plugin(mongooseFieldEncryption, { fields: ["BSN"], secret: process.env.SERVER_SECRET_MONGOOSE  });
+PatientSchema.plugin(mongooseFieldEncryption, { fields: ["BSN", "phonenumber", "adress"], secret: process.env.SERVER_SECRET_MONGOOSE  });
 
 
 module.exports = getModel('patients', PatientSchema)
