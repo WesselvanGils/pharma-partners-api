ed9332a223078fcd41cdbabdec35e6eae6e18b21
TheElementalDragon <wesselwammes@hotmail.com>
2021-12-16 12:14:38 +0100

    fix je formatting

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -1,121 +1,140 @@
- const Employee = require("../models/employee.model")()
- const config = require("../configuration/authentication.config");
- const logger = require("../configuration/config").logger;
- var jwt = require("jsonwebtoken");
- var bcrypt = require("bcryptjs");
+const Employee = require("../models/employee.model")()
+const config = require("../configuration/authentication.config");
+const logger = require("../configuration/config").logger;
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
 
 
 
- exports.signup = (req, res) => {
- 	const employee = new Employee({
- 		firstname: req.body.firstname,
- 		lastname: req.body.lastname,
- 		email: req.body.email,
- 		password: bcrypt.hashSync(req.body.password, 8),
- 		employeePrefix: req.body.employeePrefix,
- 		doctorPrefix: req.body.doctorPrefix,
- 	});
+exports.signup = (req, res) =>
+{
+	const employee = new Employee({
+		firstname: req.body.firstname,
+		lastname: req.body.lastname,
+		email: req.body.email,
+		password: bcrypt.hashSync(req.body.password, 8),
+		employeePrefix: req.body.employeePrefix,
+		doctorPrefix: req.body.doctorPrefix,
+	});
 
- 	employee.save((err, user) => {
- 		if (err) {
- 			res.status(500).send({
- 				message: err
- 			});
- 			return;
- 		}
+	employee.save((err, user) =>
+	{
+		if (err)
+		{
+			res.status(500).send({
+				message: err
+			});
+			return;
+		}
 
- 		let token = jwt.sign({
- 			id: employee.id
- 		}, config.secret, {
- 			expiresIn: 86400 // 24 hours
- 		});
+		let token = jwt.sign({
+			id: employee.id
+		}, config.secret, {
+			expiresIn: 86400 // 24 hours
+		});
 
- 		res.status(200).send({
- 			message: "employee was registered successfully!",
- 			_id: employee._id,
- 			email: employee.email,
- 			token: token
- 		});
- 	});
- };
+		res.status(200).send({
+			message: "employee was registered successfully!",
+			_id: employee._id,
+			email: employee.email,
+			token: token
+		});
+	});
+};
 
- exports.info = (req, res) => {
- 		res.status(200).json('Welkom op de API voor de F1 avans. /api/users /api/grandprixs  /api/drivers  /api/racewins  /api/constructors').end();
- 	},
+exports.info = (req, res) =>
+{
+	res.status(200).json('Welkom op de API voor de F1 avans. /api/users /api/grandprixs  /api/drivers  /api/racewins  /api/constructors').end();
+},
 
- 	exports.signin = (req, res) => {
- 		Employee.findOne({
- 				email: req.body.email
- 			})
- 			.exec((err, employee) => {
- 				if (err) {
- 					res.status(500).send({
- 						message: err
- 					});
- 					return;
- 				}
+	exports.signin = (req, res) =>
+	{
+		Employee.findOne({
+			email: req.body.email
+		})
+			.exec((err, employee) =>
+			{
+				if (err)
+				{
+					res.status(500).send({
+						message: err
+					});
+					return;
+				}
 
- 				if (!employee) {
- 					return res.status(404).send({
- 						message: "Employee Not found."
- 					});
- 				}
+				if (!employee)
+				{
+					return res.status(404).send({
+						message: "Employee Not found."
+					});
+				}
 
- 				let passwordIsValid = bcrypt.compareSync(
- 					req.body.password,
- 					employee.password
- 				);
+				let passwordIsValid = bcrypt.compareSync(
+					req.body.password,
+					employee.password
+				);
 
- 				if (!passwordIsValid) {
- 					return res.status(401).send({
- 						accessToken: null,
- 						message: "Invalid Password!"
- 					});
- 				}
+				if (!passwordIsValid)
+				{
+					return res.status(401).send({
+						accessToken: null,
+						message: "Invalid Password!"
+					});
+				}
 
- 				let token = jwt.sign({
- 					id: employee.id
- 				}, config.secret, {
- 					expiresIn: 86400 // 24 hours
- 				});
+				let token = jwt.sign({
+					id: employee.id
+				}, config.secret, {
+					expiresIn: 86400 // 24 hours
+				});
 
- 				res.status(200).send({
- 					_id: employee._id,
- 					email: employee.email,
- 					token: token
- 				});
- 			});
- 	};
+				res.status(200).send({
+					_id: employee._id,
+					email: employee.email,
+					token: token
+				});
+			});
+	};
 
 
- exports.validateToken = (req, res, next) => {
- 	logger.info("validateToken called");
- 	// logger.trace(req.headers)
+exports.validateToken = (req, res, next) =>
+{
+	logger.info("validateToken called");
+	// logger.trace(req.headers)
 
- 	const authHeader = req.headers.authorization;
- 	if (!authHeader) {
- 		logger.warn("Authorization header missing!");
- 		res.status(401).json({
- 			error: "Authorization header missing!",
- 			
- 		});
- 	} else {
- 		// Strip the word 'Bearer ' from the headervalue
- 		const token = authHeader.substring(7, authHeader.length);
+	const authHeader = req.headers.authorization;
+	if (!authHeader)
+	{
+		logger.warn("Authorization header missing!");
+		res.status(401).json({
+			error: "Authorization header missing!",
 
- 		jwt.verify(token, config.secret, (err, payload) => {
- 			if (err) {
- 				logger.warn("Not authorized");
- 				res.status(401).json({
- 					error: "Not authorized",
- 					
- 				});
- 			}
- 			if (payload) {
- 				logger.debug("token is valid", payload);
- 				req.userId = payload.id;
- 				next();
- 			}
- 		});
- 	};
- }
+		});
+	} else
+	{
+		// Strip the word 'Bearer ' from the headervalue
+		const token = authHeader.substring(7, authHeader.length);
+
+		jwt.verify(token, config.secret, (err, payload) =>
+		{
+			if (err)
+			{
+				logger.warn("Not authorized");
+				res.status(401).json({
+					error: "Not authorized",
+
+				});
+			}
+			if (payload)
+			{
+				logger.debug("token is valid", payload);
+				req.userId = payload.id;
+				res.status(202).json(
+					{
+						success: "Your token is valid"
+					}
+				);
+			}
+		});
+	};
+}
--- src/routes/authentication.routes.js
+++ src/routes/authentication.routes.js
@@ -1,15 +1,15 @@
-const {info, signup, signin, validateToken  } = require("../controllers/authentication.controller")
+const { info, signup, signin, validateToken } = require("../controllers/authentication.controller")
 const express = require("express");
 const router = express.Router();
 
- router.post('/authentication/register', signup)
+router.post('/authentication/register', signup)
 
 //  log an existing user in
- router.post("/authentication/login", signin)
+router.post("/authentication/login", signin)
 
- // log an existing user in
- router.get("/authentication/validate", validateToken )
+// log an existing user in
+router.get("/authentication/validate", validateToken)
 
-router.get("/authentication/info", info );
+router.get("/authentication/info", info);
 
 module.exports = router;
