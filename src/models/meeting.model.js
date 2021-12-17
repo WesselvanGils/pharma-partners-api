d8a8eb6257ebe098a021129cd134890d79263ad3
TheElementalDragon <wesselwammes@hotmail.com>
2021-12-17 11:55:36 +0100

    updated meeting model

--- src/models/meeting.model.js
+++ src/models/meeting.model.js
@@ -2,30 +2,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const getModel = require('./model_cache')
-const MeetingSchema = new Schema({
-
-  startDate: {
-    type: Date,
-    required: [true, "A meeting needs to have a start date."],
-  },
-  endDate: {
-    type: Date,
-    required: [true, "A meeting needs to have a end date."],
-  },
-  subject: {
-    type: String,
-    required: [true, "A meeting needs to have a subject."],
-  },
-  employee: {
-    type: Schema.Types.ObjectId,
-    ref: "employees",
-    autopopulate: true,
-  },
-  patient: {
-    type: Schema.Types.ObjectId,
-    ref: "patients",
-    autopopulate: true,
-  },
+const MeetingSchema = new Schema(
+{
+	employee: {
+		type: Schema.Types.ObjectId,
+		ref: "employees",
+		autopopulate: true,
+	},
+	patient: {
+		type: Schema.Types.ObjectId,
+		ref: "patients",
+		autopopulate: true,
+	},
+	meeting:
+	{
+		title:
+		{
+			type: String,
+			required: [ true, "" ]
+		},
+		start:
+		{
+			type: Date,
+			required: [ true, "" ]
+		},
+		end:
+		{
+			type: Date,
+			required: [ true, "" ]
+		}
+	}
 });
 
 // mongoose plugin to always populate fields
