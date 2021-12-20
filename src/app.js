a29a7f323842aa54b1d1f28cd18a0a092cacf635
TheElementalDragon <wesselwammes@hotmail.com>
2021-12-20 14:32:24 +0100

    renamed receipts to prescription

--- src/app.js
+++ src/app.js
@@ -55,13 +55,13 @@ const authRoutes = require("./routes/authentication.routes");
 const patientsRoutes = require("./routes/patient.routes");
 const meetingRoutes = require("./routes/meeting.routes")
 const medicationsRoutes = require("./routes/medication.routes")
-const receiptsRoutes = require("./routes/receipts.routes")
+const prescriptionRoutes = require("./routes/prescription.routes")
 
 app.use('/api', authRoutes)
 app.use('/api', patientsRoutes)
 app.use("/api", meetingRoutes)
 app.use("/api", medicationsRoutes)
-app.use("/api", receiptsRoutes)
+app.use("/api", prescriptionRoutes)
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const errors = require('./errors')
