9cc801c2b4c47807946b937dc68594ccd66efd37
JoeydeJonge <jd.dejonge@student.avans.nl>
2021-12-21 14:28:06 +0100

    Working example for Wessel not ready for testing yet

--- src/controllers/meeting.controller.js
+++ src/controllers/meeting.controller.js
@@ -2,5 +2,16 @@ const CrudController = require("./Crud")
 
 class MeetingController extends CrudController
 {
-    
+    getAll()
+    getAll = async (req, res, next) => {
+        const entities = await this.model.find({ employee: { $eq: _id } })
+        entities.map(entities => entities).sort();
+        res.status(200).send(entities)
+    }
 }
+
+// Equivalent to `{ rank: 'Lieutenant' }`. `$eq` is an example of
+// a "query operator"
+const docs = await Character.find({ rank: { $eq: 'Lieutenant' } });
+
+docs.map(doc => doc.name).sort(); // ['Geordi La Forge', 'Worf']
