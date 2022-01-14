24302b5fb4b2f6d9d7525c089da56f4b90eab2ad
JoeydeJonge <jd.dejonge@student.avans.nl>
2022-01-14 09:45:17 +0100

    Splunk working now

--- src/controllers/Crud.js
+++ src/controllers/Crud.js
@@ -42,10 +42,6 @@ class CrudController {
                         entity: entity,
                         employee: result,
                         method: "create",
-                        event: "Sourcetype test please",
-                        sourcetype: "testsystem",
-                        host: "some-host-123",
-                        fields: {"tracking_id": 1234}
                     }
                 
                 }
@@ -59,56 +55,42 @@ class CrudController {
         const entities = await this.model.find()
         res.status(200).send(entities)
 
-        // authcontroller.getEmployeeFromToken(req, res, next, (error, result) => {
-        //     if (error) {
-        //         console.log(error)
-        //     } else {
-        //         var payload = {
-        //             message : {
-        //                 entities: entities,
-        //                 employee: result,
-        //                 method: "getAll"
-        //             }
+        authcontroller.getEmployeeFromToken(req, res, next, (error, result) => {
+            if (error) {
+                console.log(error)
+            } else {
+                var payload = {
+                    message : {
+                        entities: entities,
+                        employee: result,
+                        method: "getAll"
+                    }
                 
-        //         }
-        //         Logger.send(payload)
-        //     }
-        // })
-    }
-
-    getBatch = async (req, res) =>
-    {
-        const startIndex = req.params.batch * req.params.amount
-        let entities = await this.model.find()
-        let response = []
-
-        await entities.sort((a, b) => {return a.patientNumber - b.patientNumber})
-        await entities.slice(startIndex, startIndex + req.params.amount).map(item =>
-        {
-            response.push(item)
+                }
+                Logger.send(payload)
+            }
         })
-        res.status(200).send(response)
     }
 
     getOne = async (req, res, next) => {
         const entity = await this.model.findById(req.params.id)
         res.status(200).send(entity)
 
-        // authcontroller.getEmployeeFromToken(req, res, next, (error, result) => {
-        //     if (error) {
-        //         console.log(error)
-        //     } else {
-        //         var payload = {
-        //             message : {
-        //                 entity: entity,
-        //                 employee: result,
-        //                 method: "getOne"
-        //             }
+        authcontroller.getEmployeeFromToken(req, res, next, (error, result) => {
+            if (error) {
+                console.log(error)
+            } else {
+                var payload = {
+                    message : {
+                        entity: entity,
+                        employee: result,
+                        method: "getOne"
+                    }
                 
-        //         }
-        //         Logger.send(payload)
-        //     }
-        // })
+                }
+                Logger.send(payload)
+            }
+        })
     }
 
 
@@ -116,21 +98,21 @@ class CrudController {
         await this.model.findByIdAndUpdate(req.params.id, req.body) 
         res.status(200).json({id: req.body._id}).end()
 
-        // authcontroller.getEmployeeFromToken(req, res, next, (error, result) => {
-        //     if (error) {
-        //         console.log(error)
-        //     } else {
-        //         var payload = {
-        //             message : {
-        //                 entity: req.body,
-        //                 employee: result,
-        //                 method: "update"
-        //             }
+        authcontroller.getEmployeeFromToken(req, res, next, (error, result) => {
+            if (error) {
+                console.log(error)
+            } else {
+                var payload = {
+                    message : {
+                        entity: req.body,
+                        employee: result,
+                        method: "update"
+                    }
                 
-        //         }
-        //         Logger.send(payload)
-        //     }
-        // })
+                }
+                Logger.send(payload)
+            }
+        })
     }
 
 
@@ -141,21 +123,21 @@ class CrudController {
         await entity.delete()
         res.status(204).end()
 
-        // authcontroller.getEmployeeFromToken(req, res, next, (error, result) => {
-        //     if (error) {
-        //         console.log(error)
-        //     } else {
-        //         var payload = {
-        //             message : {
-        //                 entity: entity,
-        //                 employee: result,
-        //                 method: "delete"
-        //             }
+        authcontroller.getEmployeeFromToken(req, res, next, (error, result) => {
+            if (error) {
+                console.log(error)
+            } else {
+                var payload = {
+                    message : {
+                        entity: entity,
+                        employee: result,
+                        method: "delete"
+                    }
                 
-        //         }
-        //         Logger.send(payload)
-        //     }
-        // })
+                }
+                Logger.send(payload)
+            }
+        })
     }
 
     
